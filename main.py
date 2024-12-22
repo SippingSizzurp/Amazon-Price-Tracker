da1718afb4962b4faa4adf355a317b4f51a98871
SippingSizzurp <111225194+SippingSizzurp@users.noreply.github.com>
2024-12-22 15:04:33 -0600

    Update main.py
    Switched from requests to AIOhttp to support asynchronous tasks

--- main.py
+++ main.py
@@ -1,42 +1,39 @@
-import requests
 from bs4 import BeautifulSoup
-import lxml
-
-
-from typing import Optional, Union
+import aiohttp
+import asyncio
 
 class Tracker:
     """Amazon product price tracker.
-    
+
     Attributes:
         url (str): Amazon product URL to track
     """
+
     def __init__(self, url: str):
         """Initialize tracker with product URL.
-        
+
         Args:
             url: Amazon product page URL
         """
         self.url = url
+        self.HEADERS = {'Accept-Language': "en-US,en;q=0.9",
+                            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35"}
 
-    def get_price(self):
-        headers = {'Accept-Language': "en-US,en;q=0.9",
-                   'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35"}
-        response = requests.get(self.url, headers=headers)
+    async def get_item_info(self) -> tuple[float, str]:
+        """     Returns:
+                [0] Price
+                [1] Title
+         """
 
-        print(response)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.url, headers=self.HEADERS) as response:
+                soup = BeautifulSoup(await response.text(), "lxml")
+                price_data = soup.find("span", class_="a-offscreen")
+                price = price_data.getText()
+                split_price = float(price.split("$")[1])
+                title = soup.find(id="productTitle").get_text().strip()
+                return split_price, title
 
-        soup = BeautifulSoup(response.content, "lxml")
-        price_data = soup.find("span", class_="a-offscreen")
-        price = price_data.getText()
-        split_price = float(price.split("$")[1])
-        title = soup.find(id="productTitle").get_text().strip()
-        print(title)
-        return split_price
-
-    def show_price(self):
-        print(self.get_price())
-
-
-
-
+    async def show_price_in_console(self) -> None: # Primarily a debugging feature
+        item_info = await self.get_item_info()
+        print(item_info[1], item_info[0])
